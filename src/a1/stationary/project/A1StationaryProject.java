b43a628359b7e471bf0c9b1d8bbe3a5c35d30c60
Basher <Basher@Basher-PC>
2015-04-30 19:06:55 +1000

    A1Stationary

---
+++ src/a1/stationary/project/A1StationaryProject.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package a1.stationary.project;
+
+/**
+ *
+ * @author Basher
+ */
+public class A1StationaryProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
